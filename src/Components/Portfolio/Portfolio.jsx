914a55f3101f05234949b9fc791c476d98aa4165
Wafi Ibrahim <wafiibrahim@me.com>
2023-09-12 19:31:25 -0400

    portfolio section commit

--- src/Components/Portfolio/Portfolio.jsx
+++ src/Components/Portfolio/Portfolio.jsx
@@ -1,7 +1,105 @@
 import React from "react";
 import "./Portfolio.css";
+import img1 from '../../Assets/Dashboard 1.png'
+
 const Portfolio = () => {
-  return <section id="portfolio"></section>;
+  return (
+    <section id="portfolio">
+
+      <h5>Personal Projects</h5>
+      <h2>Portfolio</h2>
+
+      <div className="container portfolio_container">
+
+        <article className="portfolio_item">
+
+
+          <div className="portfolio_item-image">
+
+            <img src={img1}></img>
+          </div>
+          <h3>This is a portfolio item title</h3>
+
+          <a className="btn" target="_blank">Github</a>
+          <a className="btn btn-primary" target="_blank">Live Demo</a>
+        </article>
+
+        <article className="portfolio_item">
+
+
+          <div className="portfolio_item-image">
+
+            <img src={img1}></img>
+          </div>
+          <h3>This is a portfolio item title</h3>
+
+          <a className="btn" target="_blank">Github</a>
+          <a className="btn btn-primary" target="_blank">Live Demo</a>
+        </article>
+        <article className="portfolio_item">
+
+
+          <div className="portfolio_item-image">
+
+            <img src={img1}></img>
+          </div>
+          <h3>This is a portfolio item title</h3>
+
+          <a className="btn" target="_blank">Github</a>
+          <a className="btn btn-primary" target="_blank">Live Demo</a>
+        </article>
+        <article className="portfolio_item">
+
+
+          <div className="portfolio_item-image">
+
+            <img src={img1}></img>
+          </div>
+          <h3>This is a portfolio item title</h3>
+
+          <a className="btn" target="_blank">Github</a>
+          <a className="btn btn-primary" target="_blank">Live Demo</a>
+        </article>
+
+        <article className="portfolio_item">
+
+
+          <div className="portfolio_item-image">
+
+            <img src={img1}></img>
+          </div>
+          <h3>This is a portfolio item title</h3>
+
+          <a className="btn" target="_blank">Github</a>
+          <a className="btn btn-primary" target="_blank">Live Demo</a>
+        </article>
+
+        <article className="portfolio_item">
+
+
+          <div className="portfolio_item-image">
+
+            <img src={img1}></img>
+          </div>
+          <h3>This is a portfolio item title</h3>
+
+          <div className="portfolio_item-cta">
+
+          <a className="btn" target="_blank">Github</a>
+          <a className="btn btn-primary" target="_blank">Live Demo</a>
+          </div>
+
+          
+        </article>
+
+      </div>
+
+  
+      
+      
+    
+    </section>
+  );
 };
 
 export default Portfolio;
